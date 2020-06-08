0297101367d06f8ed3aeb475b79eb14b7642a9a4
Gardusio <fabioinnabarbieri@gmail.com>
2020-06-08 17:25:35 +0200

    changing account details is now ,ridicule, but possible

--- src/main/java/my/spring/siw/tud/controllers/UserController.java
+++ src/main/java/my/spring/siw/tud/controllers/UserController.java
@@ -13,6 +13,7 @@ import my.spring.siw.tud.controllers.session.Session;
 import my.spring.siw.tud.model.Credentials;
 import my.spring.siw.tud.model.Project;
 import my.spring.siw.tud.model.Utente;
+import my.spring.siw.tud.modelServices.CredentialsService;
 import my.spring.siw.tud.modelServices.UserService;
 
 @SuppressWarnings("unused")
@@ -26,6 +27,9 @@ public class UserController {
 	@Autowired
 	private Session sessionData;
 	
+	@Autowired
+	private CredentialsService credService; // its in userController instead of CredentialController (used once)
+	
 	@RequestMapping(value="/userDetails", method = RequestMethod.GET)
 	public String showUserDetails(Model model) {
 		Credentials current = sessionData.getLoggedCredentials();
@@ -36,12 +40,14 @@ public class UserController {
 	//make it realistic
 	@RequestMapping(value="/editDetails", method = RequestMethod.POST)
 	public String editDetails(Model model, @RequestParam("username") String username, @RequestParam("password") String password) {
-		Credentials current = sessionData.getLoggedCredentials();
-		current.setPassword(password);
-		current.setUsername(username);
+		Credentials currentCredentials = sessionData.getLoggedCredentials();
+		currentCredentials.setPassword(password);
+		currentCredentials.setUsername(username);
+		
+		this.credService.saveCredentials(currentCredentials);
 		
 		model.addAttribute("changed", "Details changed");
-		model.addAttribute("currentCredentials",current);
+		model.addAttribute("currentCredentials",currentCredentials);
 		return "details"; 
 	}
 
